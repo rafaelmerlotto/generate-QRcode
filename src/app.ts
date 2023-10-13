bab828321616ba3dfa52fdca0e891f2b08f21847
root <root@Rafael-Merlotto>
2023-10-13 22:28:05 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,18 @@
 import QRCode from "qrcode";
 
 
-const btn: HTMLElement = document.getElementById('btn') as HTMLElement;
-const container: HTMLElement = document.getElementById('container') as HTMLElement;
 const input: HTMLInputElement = document.getElementById('input') as HTMLInputElement;
+const btn: HTMLElement = document.getElementById('btn') as HTMLElement;
+const container: HTMLElement = document.getElementById('qrCode') as HTMLElement;
 
 
 btn.addEventListener('click', () => {
-    const inputValue: string |any = input.value
+    const inputValue: string | any = input.value
     let stringdata = JSON.stringify(inputValue)
-console.log(stringdata)
-    console.log(inputValue)
     QRCode.toString(stringdata, { type: 'terminal' }, (err, myQRcode) => {
         if (err) return console.log(err)
-        console.log(myQRcode)
         container.innerHTML = myQRcode
     })
-
 })
 
 
